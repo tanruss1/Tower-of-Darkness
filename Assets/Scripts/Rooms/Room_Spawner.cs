42926e172bcb4f5f617fe866c8dc4c293a9a9c5e
tanruss1 <49689890+tanruss1@users.noreply.github.com>
2022-08-19 23:35:25 -0400

    Added new room types
    Added resource room and spawning room, removed graveyard room

--- Assets/Scripts/Rooms/Room_Spawner.cs
+++ Assets/Scripts/Rooms/Room_Spawner.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room_Graveyard : Room_Basic
+public class Room_Spawner : Room_Basic
 {
     [SerializeField]
     float SpawnTimer = 10f;
     [SerializeField]
     int Level = 1;
     [SerializeField]
-    GameObject Zombie;
+    GameObject Minion;
 
     // Update is called once per frame
     void Update()
@@ -23,6 +23,6 @@ public class Room_Graveyard : Room_Basic
 
     void TriggerSpawn()
     {
-        manager.SpawnMinion(Zombie, Level);
+        manager.SpawnMinion(Minion, Level);
     }
 }
