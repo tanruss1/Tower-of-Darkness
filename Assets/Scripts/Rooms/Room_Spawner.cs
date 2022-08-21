791f541dbc3560c82626b9613a3e7df37d396e12
Clyde Rollins <clyde.rollins@snhu.edu>
2022-08-21 17:28:44 -0400

    test#8

--- Assets/Scripts/Rooms/Room_Spawner.cs
+++ Assets/Scripts/Rooms/Room_Spawner.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Room_Spawner : Room_Basic
 {
+    public enum SpawnerType { Zombie, Skeleton, Goblin};
     [SerializeField]
     float MaxTimer = 10f;
     float SpawnTimer = 10f;
     [SerializeField]
     int Level = 1;
     [SerializeField]
-    GameObject Minion;
-
+    SpawnerType Minion;
 
     void Start()
     {
@@ -25,13 +25,25 @@ public class Room_Spawner : Room_Basic
         SpawnTimer -= Time.deltaTime;
         if (SpawnTimer <= 0f)
         {
-            TriggerSpawn();
             SpawnTimer = MaxTimer;
         }
     }
 
     void TriggerSpawn()
     {
-        manager.SpawnMinion(Minion, Level);
+        GameObject minion = new GameObject();
+        if (Minion == SpawnerType.Zombie)
+        {
+            //minion = new Zombie;
+        }
+        else if (Minion == SpawnerType.Skeleton)
+        {
+            //minion = new Skeleton
+        }
+        else if (Minion == SpawnerType.Goblin)
+        {
+            //minion = new Goblin
+        }
+        manager.SpawnMinion(minion, Level);
     }
 }
